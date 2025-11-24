07afb67f68fe323e97bfc988e691bda6601c28d8
anuar.iziani@n.world <anuar.iziani@n.world>
2025-11-24 12:50:07 +0100

    debug

--- src/main/java/com/localidata/extract/GenerateCSV.java
+++ src/main/java/com/localidata/extract/GenerateCSV.java
@@ -158,7 +158,7 @@ public class GenerateCSV {
 					valores[2] = valores[2].replaceAll("\"", "");
 											
 					                // **NUEVO: Filtro para procesar solo un cubo específico**
-									if (!valores[1].equals("03-030040TP")) {
+									if (!valores[1].equals("902-0204-02040101BA")) {
 										log.info("Saltando cubo " + valores[1] + " (no coincide con el filtro: " + Prop.specificCubeId + ")");
 										continue;
 									}
