8a25f01d69239ea89c15803fcca34166f0e70b7b
ivan.uroz@nfq.es <ivan.uroz@nfq.es>
2025-11-24 13:30:36 +0100

    debug

--- src/main/java/com/localidata/extract/GenerateCSV.java
+++ src/main/java/com/localidata/extract/GenerateCSV.java
@@ -158,7 +158,7 @@ public class GenerateCSV {
 					valores[2] = valores[2].replaceAll("\"", "");
 											
 					                // **NUEVO: Filtro para procesar solo un cubo específico**
-									if (!valores[1].equals("915-150005TP")) {
+									if (!valores[1].equals("02-020006TM")) {
 										log.info("Saltando cubo " + valores[1] + " (no coincide con el filtro: " + Prop.specificCubeId + ")");
 										continue;
 									}
